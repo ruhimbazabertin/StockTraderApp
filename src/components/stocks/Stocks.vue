42c3285e871130cd6c0e9a6b525d38df9791c6df
bertin <ruhimbazab@gmail.com>
2020-01-11 18:41:11 +0200

    landing page
    added header
    added component
    
    [finishes #170661428]

---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,23 @@
+<template>
+   <div>
+       <trader-stock v-for="stock in stocks" :key="stock.index" :stock="stock"></trader-stock>
+   </div>
+</template>
+
+<script>
+import Stock from '../stocks/Stock'
+export default {
+      components: {
+          'trader-stock': Stock,
+      },
+      computed: {
+        stocks() {
+            return this.$store.getters.stocks;
+        }
+      }
+}
+</script>
+
+<style>
+
+</style>
